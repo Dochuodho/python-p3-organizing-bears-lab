354b27a3f8111543dc330bf3d6620079aaa9fc70
Ochuodho <ochuodhoderrick@gmail.com>
2023-09-24 23:09:17 +0300

    COmpleted

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES ('Mr. Chocolate', 3, 'M', 'red', 'cool', 1);
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES ('Rowdy', 10, 'M', 'red', 'cool', 1);
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES ('Tabitha', 28, 'F', 'red', 'cool', 1);
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES ('Sergeant Brown', 31, 'M', 'red', 'cool', 1);
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES ('Melissa', 46, 'F', 'red', 'cool', 1);
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES ('Grinch', 5, 'M', 'red', 'cool', 1);
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES ('Wendy', 15, 'F', 'red', 'cool', 1);
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES (NULL, 20, 'M', 'red', 'cool', 1);
